2a45566ac016b30973b96b5fbb9f59189825b0ed
Mitchell Hudson <soggybag@gmail.com>
2017-06-02 10:48:51 -0700

    Add files via upload

---
+++ MSButtonNode.swift
@@ -0,0 +1,66 @@
+//
+//  MSButtonNode.swift
+//  PeeevedPenguins
+//
+//  Created by mitchell hudson on 5/23/17.
+//  Copyright © 2017 Mitchell Hudson. All rights reserved.
+//
+
+import SpriteKit
+
+enum MSButtonNodeState {
+    case MSButtonNodeStateActive, MSButtonNodeStateSelected, MSButtonNodeStateHidden
+}
+
+class MSButtonNode: SKSpriteNode {
+    
+    /* Setup a dummy action closure */
+    var selectedHandler: () -> Void = { print("No button action set") }
+    
+    /* Button state management */
+    var state: MSButtonNodeState = .MSButtonNodeStateActive {
+        didSet {
+            switch state {
+            case .MSButtonNodeStateActive:
+                /* Enable touch */
+                self.isUserInteractionEnabled = true
+                
+                /* Visible */
+                self.alpha = 1
+                break
+            case .MSButtonNodeStateSelected:
+                /* Semi transparent */
+                self.alpha = 0.7
+                break
+            case .MSButtonNodeStateHidden:
+                /* Disable touch */
+                self.isUserInteractionEnabled = false
+                
+                /* Hide */
+                self.alpha = 0
+                break
+            }
+        }
+    }
+    
+    /* Support for NSKeyedArchiver (loading objects from SK Scene Editor */
+    required init?(coder aDecoder: NSCoder) {
+        
+        /* Call parent initializer e.g. SKSpriteNode */
+        super.init(coder: aDecoder)
+        
+        /* Enable touch on button node */
+        self.isUserInteractionEnabled = true
+    }
+    
+    // MARK: - Touch handling
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        state = .MSButtonNodeStateSelected
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        selectedHandler()
+        state = .MSButtonNodeStateActive
+    }
+    
+}
